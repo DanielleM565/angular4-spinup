8b311be48ce4eb0346753877c1ff037ec1cdad4f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-10 14:17:33 -0600

    fixed a few lines in app.routes.ts that were commented out b/c of the bacon fix all orange errors gone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 //import {BaconService} from "./services/bacon-service";
 
-//export const allAppComponents = [BaconComponent, HomeComponent];
+export const allAppComponents = [HomeComponent];
 
 export const routes: Routes = [
 //	{path: "bacon", component: BaconComponent},
 	{path: "", component: HomeComponent}
 ];
 
-//export const appRoutingProviders: any[] = [BaconService];
+export const appRoutingProviders: any[] = [];
 
 export const routing = RouterModule.forRoot(routes);
 
