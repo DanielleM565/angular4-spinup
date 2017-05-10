e9d97ec5347f1bb28b93e4220abfb646ef62e34d
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-10 11:44:40 -0600

    created index.php in webpack directory

---
+++ webpack/index.php
@@ -0,0 +1,19 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>angular 4 spin up</title>
+	</head>
+	<body>
+		<angular4-spinup>Loading&hellip;</angular4-spinup>
+	</body>
+</html>
